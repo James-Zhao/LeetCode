f0cd39900e71d3de77e95780bc744cef7ca65271
James-Zhao <jameszhaosi@163.com>
2020-02-03 12:26:12 +0800

    two

---
+++ Java/src/Easy/SymmetricTree.java
@@ -0,0 +1,21 @@
+package Easy;
+
+public class SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        } else {
+            return isSymmetricTreeHelp(root.left, root.right);
+        }
+    }
+
+    private boolean isSymmetricTreeHelp(TreeNode left, TreeNode right) {
+        if (left == null || right == null) {
+            return left == right;
+        }
+        if (left.val != right.val) {
+            return false;
+        }
+        return isSymmetricTreeHelp(left.left, right.right) && isSymmetricTreeHelp(left.right, right.left);
+    }
+}
