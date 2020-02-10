fc4cc954de2683d5149a0c49cb7328c704929474
James-Zhao <jameszhaosi@163.com>
2020-02-10 18:57:56 +0800

    four question

---
+++ Java/src/Easy/AddToArrayFormOfInteger.java
@@ -0,0 +1,16 @@
+package Easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddToArrayFormOfInteger {
+    public List<Integer> addToArrayForm(int[] A, int K) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = A.length - 1; i >= 0 || K > 0 ; i--) {
+            int carry = i >= 0? A[i] + K % 10: K;
+            result.add(0, carry % 10);
+            K = i >= 0? K / 10 + carry / 10: K / 10 + 0;
+        }
+        return result;
+    }
+}
---
+++ Java/src/Easy/ArrangingCoins.java
@@ -0,0 +1,19 @@
+package Easy;
+
+public class ArrangingCoins {
+    public int arrangeCoins(int n) {
+        long nLong = (long)n;
+        long start = 0;
+        long end = nLong;
+        long mid = 0;
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (mid * (mid + 1) <= 2 * nLong) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return (int)start - 1;
+    }
+}
